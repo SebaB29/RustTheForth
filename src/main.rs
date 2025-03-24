8c710d57c62a2077c38c57a8b6c334abae937526
SebaB29 <sebita29@gmail.com>
2025-03-24 11:47:44 -0300

    Implemento Resta

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     let mut stack: Vec<i32> = vec![];
 
-    stack.push(20);
     stack.push(10);
+    stack.push(20);
 
-    apply_operation(&mut stack, "+");
+    // apply_operation(&mut stack, "+");
+    apply_operation(&mut stack, "-");
 
     println!("El último elemento de la pila es: {:?}", stack.pop());
 }
@@ -12,7 +13,7 @@ fn main() {
 fn apply_operation(stack: &mut Vec<i32>, operator: &str) {
     match operator {
         "+" => apply_sum(stack),
-        // "-" => ,
+        "-" => apply_subtraction(stack),
         // "*" => ,
         // "/" => ,
         _ => println!("Operador no reconocido, operadores admitidos: + - * /"),
@@ -20,11 +21,21 @@ fn apply_operation(stack: &mut Vec<i32>, operator: &str) {
 }
 
 fn apply_sum(stack: &mut Vec<i32>) {
-    let summand_1: Option<i32> = stack.pop();
-    let summand_2: Option<i32> = stack.pop();
+    let number_1: Option<i32> = stack.pop();
+    let number_2: Option<i32> = stack.pop();
 
-    match (summand_1, summand_2) {
+    match (number_1, number_2) {
         (Some(a), Some(b)) => stack.push(a + b),
+        _ => println!("Error: No hay suficientes elementos en la pila"),
+    }
+}
+
+fn apply_subtraction(stack: &mut Vec<i32>) {
+    let number_1: Option<i32> = stack.pop();
+    let number_2: Option<i32> = stack.pop();
+
+    match (number_1, number_2) {
+        (Some(a), Some(b)) => stack.push(a - b),
         _ => println!("Error: No hay suficientes elementos en la pila"),
     }
 }
