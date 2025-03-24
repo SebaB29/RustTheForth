6d4f4ecee423fb153fce98afd4b660e27f518f33
SebaB29 <sebita29@gmail.com>
2025-03-24 11:50:33 -0300

    Implemento Multiplicación

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     stack.push(20);
 
     // apply_operation(&mut stack, "+");
-    apply_operation(&mut stack, "-");
+    // apply_operation(&mut stack, "-");
+    apply_operation(&mut stack, "*");
 
     println!("El último elemento de la pila es: {:?}", stack.pop());
 }
@@ -14,7 +15,7 @@ fn apply_operation(stack: &mut Vec<i32>, operator: &str) {
     match operator {
         "+" => apply_sum(stack),
         "-" => apply_subtraction(stack),
-        // "*" => ,
+        "*" => apply_multiplication(stack),
         // "/" => ,
         _ => println!("Operador no reconocido, operadores admitidos: + - * /"),
     }
@@ -36,6 +37,16 @@ fn apply_subtraction(stack: &mut Vec<i32>) {
 
     match (number_1, number_2) {
         (Some(a), Some(b)) => stack.push(a - b),
+        _ => println!("Error: No hay suficientes elementos en la pila"),
+    }
+}
+
+fn apply_multiplication(stack: &mut Vec<i32>) {
+    let number_1: Option<i32> = stack.pop();
+    let number_2: Option<i32> = stack.pop();
+
+    match (number_1, number_2) {
+        (Some(a), Some(b)) => stack.push(a * b),
         _ => println!("Error: No hay suficientes elementos en la pila"),
     }
 }
