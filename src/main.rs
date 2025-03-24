2ad509ca8f441f85c15714e3dae411cf18c34d6b
SebaB29 <sebita29@gmail.com>
2025-03-24 11:55:11 -0300

    Implemento División

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     let mut stack: Vec<i32> = vec![];
 
-    stack.push(10);
     stack.push(20);
+    stack.push(10);
 
     // apply_operation(&mut stack, "+");
     // apply_operation(&mut stack, "-");
-    apply_operation(&mut stack, "*");
+    // apply_operation(&mut stack, "*");
+    apply_operation(&mut stack, "/");
 
     println!("El último elemento de la pila es: {:?}", stack.pop());
 }
@@ -16,7 +17,7 @@ fn apply_operation(stack: &mut Vec<i32>, operator: &str) {
         "+" => apply_sum(stack),
         "-" => apply_subtraction(stack),
         "*" => apply_multiplication(stack),
-        // "/" => ,
+        "/" => apply_division(stack),
         _ => println!("Operador no reconocido, operadores admitidos: + - * /"),
     }
 }
@@ -26,7 +27,7 @@ fn apply_sum(stack: &mut Vec<i32>) {
     let number_2: Option<i32> = stack.pop();
 
     match (number_1, number_2) {
-        (Some(a), Some(b)) => stack.push(a + b),
+        (Some(a), Some(b)) => stack.push(b + a),
         _ => println!("Error: No hay suficientes elementos en la pila"),
     }
 }
@@ -36,7 +37,7 @@ fn apply_subtraction(stack: &mut Vec<i32>) {
     let number_2: Option<i32> = stack.pop();
 
     match (number_1, number_2) {
-        (Some(a), Some(b)) => stack.push(a - b),
+        (Some(a), Some(b)) => stack.push(b - a),
         _ => println!("Error: No hay suficientes elementos en la pila"),
     }
 }
@@ -46,7 +47,17 @@ fn apply_multiplication(stack: &mut Vec<i32>) {
     let number_2: Option<i32> = stack.pop();
 
     match (number_1, number_2) {
-        (Some(a), Some(b)) => stack.push(a * b),
+        (Some(a), Some(b)) => stack.push(b * a),
+        _ => println!("Error: No hay suficientes elementos en la pila"),
+    }
+}
+
+fn apply_division(stack: &mut Vec<i32>) {
+    let number_1: Option<i32> = stack.pop();
+    let number_2: Option<i32> = stack.pop();
+
+    match (number_1, number_2) {
+        (Some(a), Some(b)) => stack.push(b / a),
         _ => println!("Error: No hay suficientes elementos en la pila"),
     }
 }
