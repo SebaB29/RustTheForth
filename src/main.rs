1462b370e9424c00873edc7e3e1a5571c8c97069
SebaB29 <sebita29@gmail.com>
2025-03-24 15:45:43 -0300

    Implemento struct Stack y le pongo tamaño definido

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,67 @@
+struct Stack {
+    elements: Vec<i16>,
+    max_size: usize,
+}
+
+impl Stack {
+    fn new(max_size_in_bytes: usize) -> Self {
+        let max_size: usize = max_size_in_bytes / 2;
+        Stack {
+            elements: Vec::with_capacity(max_size),
+            max_size,
+        }
+    }
+
+    fn push(&mut self, value: i16) {
+        if self.len() + 1 < self.max_size {
+            self.elements.push(value);
+        } else {
+            println!("La pila esta llena.");
+        }
+    }
+
+    fn pop(&mut self) -> Option<i16> {
+        if !self.is_empty() {
+            self.elements.pop()
+        } else {
+            println!("La pila está vacía.");
+            Some(0)
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.elements.len()
+    }
+
+    fn peek(&self) -> Option<&i16> {
+        self.elements.last()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.elements.is_empty()
+    }
+}
+
 fn main() {
-    let mut stack: Vec<i16> = vec![];
+    let mut stack = Stack::new(128*1024);
 
     stack.push(20);
     stack.push(10);
 
-    // apply_operation(&mut stack, "+");
+    apply_operation(&mut stack, "+");
     // apply_operation(&mut stack, "-");
     // apply_operation(&mut stack, "*");
-    apply_operation(&mut stack, "/");
+    // apply_operation(&mut stack, "/");
 
-    println!("El último elemento de la pila es: {:?}", stack.pop());
+    println!("El último elemento de la pila es: {:?}", stack.peek());
 }
 
-fn apply_operation(stack: &mut Vec<i16>, operator: &str) {
+fn apply_operation(stack: &mut Stack, operator: &str) {
+    if stack.len() < 2 {
+        println!("Operadores Insuficientes.");
+        return;
+    }
+
     match operator {
         "+" => apply_sum(stack),
         "-" => apply_subtraction(stack),
@@ -22,7 +71,7 @@ fn apply_operation(stack: &mut Vec<i16>, operator: &str) {
     }
 }
 
-fn apply_sum(stack: &mut Vec<i16>) {
+fn apply_sum(stack: &mut Stack) {
     let number_1: Option<i16> = stack.pop();
     let number_2: Option<i16> = stack.pop();
 
@@ -32,7 +81,7 @@ fn apply_sum(stack: &mut Vec<i16>) {
     }
 }
 
-fn apply_subtraction(stack: &mut Vec<i16>) {
+fn apply_subtraction(stack: &mut Stack) {
     let number_1: Option<i16> = stack.pop();
     let number_2: Option<i16> = stack.pop();
 
@@ -42,7 +91,7 @@ fn apply_subtraction(stack: &mut Vec<i16>) {
     }
 }
 
-fn apply_multiplication(stack: &mut Vec<i16>) {
+fn apply_multiplication(stack: &mut Stack) {
     let number_1: Option<i16> = stack.pop();
     let number_2: Option<i16> = stack.pop();
 
@@ -52,7 +101,7 @@ fn apply_multiplication(stack: &mut Vec<i16>) {
     }
 }
 
-fn apply_division(stack: &mut Vec<i16>) {
+fn apply_division(stack: &mut Stack) {
     let number_1: Option<i16> = stack.pop();
     let number_2: Option<i16> = stack.pop();
 
