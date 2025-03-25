e05c45144c6efbc5bea8f9501c5adbf228cdbcf2
SebaB29 <sebita29@gmail.com>
2025-03-25 16:04:18 -0300

    Lectura desde archivo fth y parámetros por cmd

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,10 @@
 mod operations;
+mod program;
 mod stack;
 
-use operations::apply_operation;
-use stack::Stack;
-
 fn main() {
-    let mut stack = Stack::new(128 * 1024);
-    let input = "25 10 + 3 * CR .";
-    execute(&mut stack, input);
-}
-
-fn execute(stack: &mut Stack, input: &str) {
-    for token in input.split_whitespace() {
-        match token {
-            "+" | "-" | "*" | "/" => apply_operation(stack, token),
-            "CR" => println!(),
-            "." => {
-                if let Some(value) = stack.pop() {
-                    println!("{}", value);
-                }
-            }
-            _ => {
-                if let Ok(number) = token.parse::<i16>() {
-                    stack.push(number);
-                } else {
-                    println!("Error: Token no reconocido '{}'", token);
-                }
-            }
-        }
+    match program::parse_args() {
+        Ok((filename, stack_size)) => program::execute_program(stack_size, filename),
+        Err(error_msg) => println!("{}", error_msg),
     }
 }
---
+++ src/program.rs
@@ -0,0 +1,73 @@
+use crate::operations::apply_operation;
+use crate::stack::Stack;
+use std::env;
+use std::fs;
+
+const DEFAULT_STACK_SIZE: usize = 128 * 1024;
+
+pub fn parse_args() -> Result<(String, usize), String> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return Err("Error: Debes especificar un archivo .fth".to_string());
+    }
+
+    let filename = args[1].clone();
+    let stack_size = parse_stack_size(&args);
+
+    Ok((filename, stack_size))
+}
+
+pub fn execute_program(stack_size: usize, filename: String) {
+    let mut stack = Stack::new(stack_size);
+    match read_file(filename) {
+        Ok(content) => {
+            execute(&mut stack, content);
+        }
+        Err(error_msg) => println!("{}", error_msg),
+    }
+}
+
+fn parse_stack_size(args: &[String]) -> usize {
+    for arg in args {
+        if arg.starts_with("stack-size=") {
+            if let Some(size_str) = arg.split('=').nth(1) {
+                if let Ok(size) = size_str.parse::<usize>() {
+                    return size;
+                }
+            }
+        }
+    }
+    DEFAULT_STACK_SIZE
+}
+
+fn read_file(filename: String) -> Result<String, String> {
+    match fs::read_to_string(&filename) {
+        Ok(content) => Ok(content),
+        Err(error_msg) => Err(format!(
+            "Error al leer el archivo '{}': {}",
+            filename, error_msg
+        )),
+    }
+}
+
+fn execute(stack: &mut Stack, input: String) {
+    for token in input.split_whitespace() {
+        match token {
+            "+" | "-" | "*" | "/" => apply_operation(stack, token),
+            "CR" => println!(),
+            "." => {
+                if let Some(value) = stack.pop() {
+                    println!("{}", value);
+                }
+            }
+            _ => {
+                if let Ok(number) = token.parse::<i16>() {
+                    stack.push(number);
+                } else {
+                    println!("Error: Token no reconocido '{}'", token);
+                }
+            }
+        }
+    }
+}
